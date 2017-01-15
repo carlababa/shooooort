8f104edfd5dd489fc501afc6a6055ccf1b652d66
Carla Babadopulos <cacababa@gmail.com>
2017-01-15 09:28:23 +0100

    Create form to POST url

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,62 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      shortenedLinks: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.shortenUrl = this.shortenUrl.bind(this);
+    this.addLink = this.addLink.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ url: event.target.value });
+  }
+
+  addLink(response) {
+    this.setState({
+      shortenedLinks: [
+        ...this.state.shortenedLinks,
+        {
+          longUrl: this.state.url,
+          shortcode: response.data.shortcode,
+        },
+      ],
+      url: '',
+    });
+  }
+
+  shortenUrl(event) {
+    event.preventDefault();
+    const urlApi = 'http://localhost:3000/proxy/shorten';
+    const url = this.state.url;
+
+    axios.post(urlApi, { url })
+      .then(this.addLink)
+      .catch((error) => {
+        window.alert(error); // eslint-disable-line no-alert
+      });
+  }
+
   render() {
     return (
-      <div>Hello React</div>
+      <div>
+        <form onSubmit={this.shortenUrl}>
+          <input
+            type="text"
+            placeholder="Paste the link you want to shorten here"
+            value={this.state.url} onChange={this.handleChange}
+          />
+          <button type="submit">
+            Shorten this link
+          </button>
+        </form>
+      </div>
     );
   }
 }
